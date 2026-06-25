4dc0f83e5c867ffbf52a18357aafee4559dfac7a
serii burduja <seriiburduja@gmail.com>
2026-06-25 17:59:21 +0300

    feat: start question card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { QuestionCard } from "./components/QuestionCard";
+import { questions } from "./data/questions";
+
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center flex-col">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-purple-500 mb-2">Quiz App</h1>
+        <p className="text-gray-400">Test you knowledge</p>
+        <QuestionCard data={questions[currentQuestion]} />
+      </div>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { IQuestion } from "../data/questions";
+
+interface Props {
+  data: IQuestion;
+}
+
+export const QuestionCard: React.FC<Props> = ({ data }) => {
+  const { question, options, answer } = data;
+  return <div>some</div>;
+};

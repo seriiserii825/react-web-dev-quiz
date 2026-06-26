80cba2341c4ad5d86d8870c977834a91de1384ae
serii burduja <seriiburduja@gmail.com>
2026-06-26 10:07:38 +0300

    feat: select answer after disable

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,32 @@ import { questions } from "./data/questions";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+
+  const [selectedAnswer, setSelectedAnswer] = useState<null | string>(null);
+  const [score, setScore] = useState<number>(0);
+  const [isFinished, setIsFinished] = useState<boolean>(false);
+  const [wasSelected, setWasSelected] = useState<boolean>(false);
+
+  function handleAnswer(option: string) {
+    if (wasSelected) return;
+    setSelectedAnswer(option);
+    setWasSelected(true);
+
+    if (option === questions[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+  }
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center flex-col">
+    <div className="min-h-screen bg-gray-900 px-4 text-white flex items-center justify-center flex-col">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-purple-500 mb-2">Quiz App</h1>
-        <p className="text-gray-400">Test you knowledge</p>
-        <QuestionCard data={questions[currentQuestion]} />
+        <p className="text-gray-400 mb-4">Test you knowledge</p>
+        <div className="mb-4 p-4">Score: {score}</div>
+        <QuestionCard
+          data={questions[currentQuestion]}
+          wasSelected={wasSelected}
+          onAnswer={handleAnswer}
+        />
       </div>
     </div>
   );
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import type { IQuestion } from "../data/questions";
 
 interface Props {
   data: IQuestion;
+  onAnswer: (option: string) => void;
+  wasSelected: boolean;
 }
 
-export const QuestionCard: React.FC<Props> = ({ data }) => {
+export const QuestionCard: React.FC<Props> = ({ data, onAnswer, wasSelected }) => {
   const { question, options, answer } = data;
-  return <div>{question}</div>;
+
+  return (
+    <div className="bg-gray-800 p-6 rounded-2xl shadow-lg w-full max-w-2xl border-gray-700">
+      <p className="mb-4">{question}</p>
+      <div className="flex flex-col gap-3">
+        {options.map((option, index) => (
+          <button
+            onClick={() => onAnswer(option)}
+            className="text-left px-4 py-3 cursor-pointer bg-blue-700 text-white rounded-lg disabled:opacity-75 disabled:cursor-not-allowed"
+            key={index}
+            disabled={wasSelected}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
